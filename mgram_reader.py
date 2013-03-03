6b65c85018105b102004c3aa81b606bbcded25cb
Wojciech Sznapka <wojciech@sznapka.pl>
2013-03-03 19:15:37 +0100

    Refactored to be object oriented

--- mgram_reader.py
+++ mgram_reader.py
@@ -3,62 +3,78 @@
 from PIL import Image
 from datetime import datetime,timedelta
 import os
+import urllib
 
-i = Image.open('/home/wowo/Downloads/mgram_pict.png', 'r').convert('RGB')
+class TemperatureReader:
 
-x_range = range(79, 477)
-y_range = range(30, 133)
-pixel = (255, 0, 0)
-temperatures = []
-scale_x = 64
-time_y = 61
-scale = []
-times = []
-
-
-def ocr_image(image):
-    file = '/tmp/ocr_image.png'
-    image.save(file)
-    string = os.popen('gocr -C \'0123456789--\' -i %s' % file).read()
-    os.remove(file)
-
-    return string
-
-for x in range(scale_x, x_range[-1]):
+    x_range = range(79, 477)
+    y_range = range(30, 133)
+    pixel = (255, 0, 0)
+    scale_x = 64
+    time_y = 61
     step = 180
-    if (0, 0, 0) == i.getpixel((x, time_y)):
-        if len(times) == 0:
-            number = i.crop((x - 8, time_y - 20, x + 8, time_y - 8))
-            string =  int(ocr_image(number))
-            date = datetime.now()
-            times.append((x, date.replace(hour=int(), minute=0, second=0, microsecond=0)))
-        else:
-            times.append((x, (times[-1][1] + timedelta(minutes=step))))
+    image = None
 
-for y in y_range:
-    if (0, 0, 0) == i.getpixel((scale_x, y)) and y - 1 not in scale:
-        number = i.crop((scale_x - 27, y - 4, scale_x - 7, y + 5))
-        scale.append((y, float(ocr_image(number))))
+    def __init__(self, image):
+        self.image = image
 
-for x in x_range:
-    for y in y_range:
-        if pixel == i.getpixel((x,y)):
-            prev = None
-            for touple in scale:
-                if touple[0] >= y:
-                    temperature = round((touple[1] - prev[1]) * (float(y - prev[0]) / float(touple[0] - prev[0])), 1)
-                    if touple[1] >= 0.0:
-                        temperature = temperature * -1.0
+    def __ocr_image(self, image):
+        file = '/tmp/ocr_image.png'
+        image.save(file)
+        string = os.popen('gocr -C \'0123456789--\' -i %s' % file).read()
+
+        return string
+
+    def __get_times(self):
+        times = []
+        for x in range(self.scale_x, self.x_range[-1]):
+            if (0, 0, 0) == self.image.getpixel((x, self.time_y)):
+                if len(times) == 0:
+                    number = self.image.crop((x - 8, self.time_y - 20, x + 8, self.time_y - 8))
+                    string =  int(self.__ocr_image(number))
+                    date = datetime.now()
+                    times.append((x, date.replace(hour=int(), minute=0, second=0, microsecond=0)))
+                else:
+                    times.append((x, (times[-1][1] + timedelta(minutes=self.step))))
+
+        return times
+
+    def __get_scale(self):
+        scale = []
+        for y in self.y_range:
+            if (0, 0, 0) == self.image.getpixel((self.scale_x, y)) and y - 1 not in scale:
+                number = self.image.crop((self.scale_x - 27, y - 4, self.scale_x - 7, y + 10))
+                scale.append((y, float(self.__ocr_image(number))))
+
+        return scale
+
+    def read(self):
+        temperatures = []
+        for x in self.x_range:
+            for y in self.y_range:
+                if self.pixel == self.image.getpixel((x,y)):
+                    prev = None
+                    for touple in self.__get_scale():
+                        if touple[0] >= y:
+                            temperature = round((touple[1] - prev[1]) * (float(y - prev[0]) / float(touple[0] - prev[0])), 1)
+                            if touple[1] >= 0.0:
+                                temperature = temperature * -1.0
+                            break
+                        prev = touple
+                    prev = None
+                    for touple in self.__get_times():
+                        if touple[0] >= x:
+                            minutes = round(self.step * (float(x - prev[0]) / float(touple[0] - prev[0])))
+                            time = prev[1] + timedelta(minutes=minutes)
+                        prev = touple
+                    temperatures.append((time,temperature))
                     break
-                prev = touple
-            prev = None
-            for touple in times:
-                if touple[0] >= x:
-                    minutes = round(step * (float(x - prev[0]) / float(touple[0] - prev[0])))
-                    time = prev[1] + timedelta(minutes=minutes)
-                prev = touple
-            temperatures.append((time,temperature))
-            break
 
+        return temperatures
 
-print temperatures
+row = 463
+col = 211
+file = '/tmp/mgram_%d_%d.png' % (row, col)
+urllib.urlretrieve('http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=%d&col=%d&lang=pl' % (row, col), file)
+reader = TemperatureReader(Image.open(file, 'r').convert('RGB'))
+print reader.read()
