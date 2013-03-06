d795c73ae163946a9fba6f85d26ba1345cc453e2
Wojciech Sznapka <wojtek@sznapka.pl>
2013-03-06 11:41:54 +0100

    Added connection with mongo, store method (saving / updating forecast), reading temperatures from db

--- api.py
+++ api.py
@@ -2,31 +2,80 @@
 
 from PIL import Image
 from datetime import datetime, timedelta
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from mgram_reader import TemperatureReader
+from pymongo import MongoClient
 import json
+import os
 import sys
 import urllib
-import os
+import yaml
 
 app = Flask(__name__)
 
-@app.route('/<int:row>/<int:col>')
-def fetch(row, col):
-    delta = datetime.now() - timedelta(hours=3)
+@app.route('/<int:rows>/<int:cols>')
+def fetch(rows, cols):
+    start = datetime.now() - timedelta(days=int(request.args.get('days', 2)))
+    result = getCollection().find({'date': {'$gte': start}, 'location.rows': rows, 'location.cols': cols}).sort('date', 1)
+    print result.count()
+    data = [('Data', 'Prognoza')]
+    for temperature in result:
+        data.append((temperature['date'].strftime('%m-%d %H:%M'), float(temperature['temperature'])))
 
-    file = '/tmp/mgram_%d_%d.png' % (row, col)
-    if not os.path.exists(file) or datetime.fromtimestamp(os.path.getmtime(file)) < delta:
-        urllib.urlretrieve('http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=%d&col=%d&lang=pl' % (row, col), file)
+    return json.dumps(data)
 
-    reader = TemperatureReader(Image.open(file, 'r').convert('RGB'))
 
-    return json.dumps([['Data', 'Prognoza']] + reader.read(date_as_string=True))
-
-@app.route('/chart')
+@app.route('/')
 def chart():
     return render_template('chart.html')
+
+
+def store(rows, cols):
+    collection = getCollection()
+    dates = []
+    for row in getReader(rows, cols).read()[1:]:
+        date = row[0].replace(minute = ((row[0].minute / 10) * 10))
+        if date not in dates:
+            dates.append(date)
+            document = collection.find_one({
+                'date': date,
+                'location.rows': rows,
+                'location.cols': cols,
+                'temperature': {'$ne': row[1]}
+            })
+            if not document:
+                document = {
+                    'date': date,
+                    'location': {'rows': rows, 'cols': cols},
+                    'temperature': row[1]
+                }
+            collection.save(document)
+
+
+def getReader(rows, cols):
+    delta = datetime.now() - timedelta(hours=3)
+    file = '/tmp/mgram_%d_%d.png' % (rows, cols)
+    if not os.path.exists(file) or datetime.fromtimestamp(os.path.getmtime(file)) < delta:
+        urllib.urlretrieve('http://new.meteo.pl/um/metco/mgram_pict.php?ntype=0u&row=%d&col=%d&lang=pl' % (rows, cols), file)
+
+    return TemperatureReader(Image.open(file, 'r').convert('RGB'))
+
+
+def getCollection():
+    config = getConfig()['database']
+    conn = MongoClient(config['host'], config['port'])
+    db = conn[config['collection']]
+    db.authenticate(config['user'], config['pass'])
+
+    return db.temperatures
+
+
+def getConfig():
+  return yaml.load(file(os.path.dirname(os.path.realpath(__file__)) + '/config.yml'))
+
 
 if __name__ == "__main__" and len(sys.argv) == 1:
     #app.run(host='0.0.0.0', debug=True)
     app.run(host='91.227.39.112', port=8000, debug=True)
+elif sys.argv[1] == '--store':
+    store(int(sys.argv[2]), int(sys.argv[3]))
